4978c8f3e149a4d34476a819089727ac0bc9ad3f
Dmlehan <dmlehanamanda@gmail.com>
2023-08-22 15:53:21 +0530

    varible type

---
+++ 1stLab/3stVariableType.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int num=45;
+    double decimal=10.23;
+    char letter ='a';
+    bool isSunny = false ;
+    letter= 'a' ;
+
+     cout<< "01 value: " <<num<< endl << "02 value : "<<  decimal<< endl <<"03 value : "<< letter<< endl << "04 value: " << isSunny<<endl<<"05 value : "<<letter ;
+     
+     
+    
+}
