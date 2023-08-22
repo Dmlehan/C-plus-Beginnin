6bf0db6bdac1bdce8cab840beb08fed9d72df3ff
Dmlehan <dmlehanamanda@gmail.com>
2023-08-22 16:13:27 +0530

    if condition (if else)

---
+++ 1stLab/4ifCondition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+int num;
+cout << "Enter number : ";
+cin>>num;
+if(num>20){
+    cout<<num << " number is greater than 20";
+}else{
+     cout<<num <<" number is less than 20 ";
+     }
+    return 0;
+
+}
