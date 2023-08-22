295be59fc9d571ceb7b4e490c04be6b6a54b0bf8
Dmlehan <dmlehanamanda@gmail.com>
2023-08-22 14:14:57 +0530

    calculation

---
+++ 1stLab/2st.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2,ans ;
+    cout<< "Enter first number: " ;
+    cin >> num1;
+     cout<<"Enter second numbebr: ";
+    cin>> num2;
+    ans=num1+num2;
+    cout<<"The answer is : "<< ans;
+    return 0;
+
+}
